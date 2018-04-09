d3a80b15eaf6cb2765fe16f6ec2f01bddcc819d5
johnlindquist <johnlindquist@gmail.com>
2018-04-09 10:28:17 -0600

    passing handlers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from "react"
 import { render } from "react-dom"
-import { pipeProps } from "react-streams"
-import { pluck, switchMap } from "rxjs/operators"
-import { ajax } from "rxjs/ajax"
+import { pipeProps, sourceNext } from "react-streams"
+import { map, scan, startWith } from "rxjs/operators"
 
-const PersonLoader = pipeProps(pluck("url"), switchMap(ajax), pluck("response"))
+const Counter = pipeProps(() => {
+  const [click$, onClick] = sourceNext()
 
-const Person = props => (
-  <div>
-    <h1>{props.name}</h1>
-    <img
-      src={`https://azure-lipstick.glitch.me/${props.image}`}
-      alt={props.name}
-    />
-  </div>
-)
+  return click$.pipe(
+    startWith(0),
+    scan(count => count + 1),
+    map(count => ({ count, onClick }))
+  )
+})
 
 render(
-  <PersonLoader url="https://azure-lipstick.glitch.me/people/10">
-    {Person}
-  </PersonLoader>,
+  <Counter>
+    {props => (
+      <div>
+        <h1>{props.count}</h1>
+        <button onClick={props.onClick}>+</button>
+      </div>
+    )}
+  </Counter>,
   document.querySelector("#root")
 )
