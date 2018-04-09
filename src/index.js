02bfa8897c4795aec8c7f8fefb39ade9c95f8951
johnlindquist <johnlindquist@gmail.com>
2018-04-09 10:27:38 -0600

    ajax

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react"
 import { render } from "react-dom"
 import { pipeProps } from "react-streams"
-import { interval } from "rxjs"
-import { map, switchMap } from "rxjs/operators"
+import { pluck, switchMap } from "rxjs/operators"
+import { ajax } from "rxjs/ajax"
 
-const Timer = pipeProps(
-  switchMap(props => interval(props.time)),
-  map(tick => ({ tick }))
+const PersonLoader = pipeProps(pluck("url"), switchMap(ajax), pluck("response"))
+
+const Person = props => (
+  <div>
+    <h1>{props.name}</h1>
+    <img
+      src={`https://azure-lipstick.glitch.me/${props.image}`}
+      alt={props.name}
+    />
+  </div>
 )
 
 render(
-  <Timer time={250}>{props => <h1>{props.tick}</h1>}</Timer>,
+  <PersonLoader url="https://azure-lipstick.glitch.me/people/10">
+    {Person}
+  </PersonLoader>,
   document.querySelector("#root")
 )
