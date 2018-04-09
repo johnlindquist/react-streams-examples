af7ace3e13f84a22ac0fa63e2eced24a2ae8bd4d
johnlindquist <johnlindquist@gmail.com>
2018-04-09 10:27:13 -0600

    start a stream from props

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react"
 import { render } from "react-dom"
 import { pipeProps } from "react-streams"
 import { interval } from "rxjs"
-import { map } from "rxjs/operators"
+import { map, switchMap } from "rxjs/operators"
 
-const Timer = pipeProps(() => interval(1000).pipe(map(tick => ({ tick }))))
+const Timer = pipeProps(
+  switchMap(props => interval(props.time)),
+  map(tick => ({ tick }))
+)
 
 render(
-  <Timer>{props => <h1>{props.tick}</h1>}</Timer>,
+  <Timer time={250}>{props => <h1>{props.tick}</h1>}</Timer>,
   document.querySelector("#root")
 )
