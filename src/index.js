ba7294f6a306cb64de212ec2dd6063ee40bf7557
johnlindquist <johnlindquist@gmail.com>
2018-04-11 15:56:53 -0600

    refactor to source

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react"
 import { render } from "react-dom"
-import { pipeProps, sourceNext } from "react-streams"
+import { pipeProps, source } from "react-streams"
 import { merge } from "rxjs"
 import { pluck, mergeMap, map, mapTo, startWith, scan } from "rxjs/operators"
 
 const Stepper = pipeProps(
   mergeMap(({ defaultValue, step, min, max }) => {
-    const [dec$, onDec] = sourceNext(mapTo(v => v - step))
-    const [inc$, onInc] = sourceNext(mapTo(v => v + step))
-    const [change$, onChange] = sourceNext(
-      pluck("target", "value"),
-      map(x => () => x)
-    )
+    const onDec = source(mapTo(v => v - step))
+    const onInc = source(mapTo(v => v + step))
+    const onChange = source(pluck("target", "value"), map(x => () => x))
 
     const clamp = value => (value > max ? max : value < min ? min : value)
 
-    return merge(dec$, inc$, change$).pipe(
+    return merge(onDec, onInc, onChange).pipe(
       startWith(defaultValue),
       scan((acc, fn) => clamp(fn(acc))),
       map(value => ({ value, onDec, onInc, onChange }))
