745c74bcdb4d4057754863f0e7ebd8266050f9f4
johnlindquist <johnlindquist@gmail.com>
2018-04-11 16:46:23 -0600

    refactor to source

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { render } from "react-dom"
-import { pipeProps, sourceNext } from "react-streams"
+import { pipeProps, source } from "react-streams"
 import { map, pluck, startWith, switchMap } from "rxjs/operators"
 
 const ToggleCheckbox = pipeProps(
   pluck("checked"),
   switchMap(checked => {
-    const [change$, onChange] = sourceNext(pluck("target", "checked"))
+    const onChange = source(pluck("target", "checked"))
 
-    return change$.pipe(
+    return onChange.pipe(
       startWith(checked),
       map(checked => ({ checked, onChange }))
     )
