4ec57c652fe521fc2ba14b3e739af504c8aaaee9
johnlindquist <johnlindquist@gmail.com>
2018-04-11 16:32:42 -0600

    refactor to source

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { render } from "react-dom"
-import { pipeProps, sourceNext } from "react-streams"
+import { pipeProps, source } from "react-streams"
 import { map, startWith, pluck, switchMap } from "rxjs/operators"
 
 const TypingDemo = pipeProps(
   pluck("text"),
   switchMap(text => {
-    const [input$, onInput] = sourceNext(pluck("target", "value"))
+    const onInput = source(pluck("target", "value"))
 
-    return input$.pipe(startWith(text), map(text => ({ text, onInput })))
+    return onInput.pipe(startWith(text), map(text => ({ text, onInput })))
   })
 )
 render(
