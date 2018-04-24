9fd350922ae93267eba2dfea62514ae12474db29
johnlindquist <johnlindquist@gmail.com>
2018-04-24 18:27:56 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+//#region imports
 import React from "react"
-import { from, interval, combineLatest, merge } from "rxjs"
+import {
+  from,
+  interval,
+  combineLatest,
+  merge
+} from "rxjs"
 import { render } from "react-dom"
 import { pipeProps, source } from "react-streams"
 import {
@@ -9,56 +15,75 @@ import {
   takeWhile,
   share,
   mapTo,
-  filter
+  filter,
+  switchMap
 } from "rxjs/operators"
-
-const Alarm = pipeProps(props$ => {
-  const snooze = source(startWith(7), mapTo(7))
-  const dismiss = source()
-
-  const makeCountdown = start =>
-    interval(250).pipe(
+//#endregion
+const Alarm = pipeProps(
+  switchMap(({ start }) => {
+    const snooze = source(
       startWith(start),
-      scan(time => time - 1),
-      takeWhile(time => time >= 0)
+      mapTo(start)
+    )
+    const dismiss = source()
+
+    const makeCountdown = start =>
+      interval(250).pipe(
+        startWith(start),
+        scan(time => time - 1),
+        takeWhile(time => time >= 0)
+      )
+
+    const countdown$ = from(snooze).pipe(
+      exhaustMap(makeCountdown),
+      share()
     )
 
-  const countdown$ = from(snooze).pipe(exhaustMap(makeCountdown), share())
+    const zero$ = countdown$.pipe(
+      filter(v => v === 0)
+    )
 
-  const zero$ = countdown$.pipe(filter(v => v === 0))
+    const message$ = merge(
+      countdown$,
+      zero$.pipe(mapTo("Wake up! 🎉")),
+      from(dismiss).pipe(
+        mapTo("Have a nice day! 🤗")
+      )
+    )
 
-  const message$ = merge(
-    countdown$,
-    zero$.pipe(mapTo("Wake up! 🎉")),
-    from(dismiss).pipe(mapTo("Have a nice day! 🤗"))
-  )
+    const areButtonsDisabled$ = merge(
+      merge(snooze, dismiss).pipe(mapTo(true)),
+      zero$.pipe(mapTo(false))
+    )
 
-  const areButtonsDisabled$ = merge(
-    merge(snooze, dismiss).pipe(mapTo(true)),
-    zero$.pipe(mapTo(false))
-  )
-
-  return combineLatest(
-    message$,
-    areButtonsDisabled$,
-    (message, areButtonsDisabled) => ({
-      message,
-      areButtonsDisabled,
-      snooze,
-      dismiss
-    })
-  )
-})
+    return combineLatest(
+      message$,
+      areButtonsDisabled$,
+      (message, areButtonsDisabled) => ({
+        message,
+        areButtonsDisabled,
+        snooze,
+        dismiss
+      })
+    )
+  })
+)
 
 render(
-  <Alarm>
+  <Alarm start={7}>
     {props => (
       <div>
         <h2>{props.message}</h2>
-        <button disabled={props.areButtonsDisabled} onClick={props.snooze}>
+        <button
+          disabled={props.areButtonsDisabled}
+          onClick={props.snooze}
+        >
           Snooze
         </button>
-        <button disabled={props.areButtonsDisabled} onClick={props.dismiss}>
+        <button
+          disabled={props.areButtonsDisabled}
+          onClick={props.dismiss}
+        >
           Dismiss
         </button>
       </div>
