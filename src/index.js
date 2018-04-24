9db480b65362b06d9d23843543c346ed75026af9
johnlindquist <johnlindquist@gmail.com>
2018-04-24 21:49:12 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -2,32 +2,38 @@ import React from "react"
 import { render } from "react-dom"
 import { pipeProps, source } from "react-streams"
 import { from } from "rxjs"
-import { map, pluck, startWith, switchMap } from "rxjs/operators"
+import {
+  map,
+  startWith,
+  switchMap,
+  scan
+} from "rxjs/operators"
 
-const ToggleCheckbox = pipeProps(
-  pluck("checked"),
-  switchMap(checked => {
-    const onChange = source(pluck("target", "checked"))
-
-    return from(onChange).pipe(
-      startWith(checked),
-      map(checked => ({ checked, onChange }))
+const Toggle = pipeProps(
+  switchMap(({ on }) => {
+    const onChange = source(
+      startWith(on),
+      scan(bool => !bool)
     )
+
+    return onChange.pipe(map(on => ({ on, onChange })))
   })
 )
 
 render(
-  <ToggleCheckbox checked={true}>
+  <Toggle on={true}>
     {props => (
       <div>
         <input
           type="checkbox"
           onChange={props.onChange}
-          checked={props.checked}
+          checked={props.on}
         />
-        <h1>{props.checked ? "ON" : "OFF"}</h1>
+        <h1 onClick={props.onChange}>
+          {props.on ? "ON" : "OFF"}
+        </h1>
       </div>
     )}
-  </ToggleCheckbox>,
+  </Toggle>,
   document.querySelector("#root")
 )
