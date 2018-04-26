ba6c71b74984209f4f5ad5eb7b6272c711779685
johnlindquist <johnlindquist@gmail.com>
2018-04-26 23:41:33 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import React from "react"
 import { render } from "react-dom"
 import { pipeProps } from "react-streams"
-import { pluck, switchMap } from "rxjs/operators"
+import { map, switchMap } from "rxjs/operators"
 import { ajax } from "rxjs/ajax"
+const URL = `https://swapi.glitch.me`
 
-const PersonLoader = pipeProps(pluck("url"), switchMap(ajax), pluck("response"))
-
-const Person = props => (
-  <div>
-    <h1>{props.name}</h1>
-    <img
-      src={`https://azure-lipstick.glitch.me/${props.image}`}
-      alt={props.name}
-    />
-  </div>
+const StarWars = pipeProps(
+  switchMap(({ url, type, id }) =>
+    ajax(`${url}/${type}/${id}`).pipe(
+      map(({ response }) => ({
+        person: response,
+        url
+      }))
+    )
+  )
 )
 
 render(
-  <PersonLoader url="https://azure-lipstick.glitch.me/people/10">
-    {Person}
-  </PersonLoader>,
+  <StarWars url={URL} type="people" id={0}>
+    {({ person, url }) => (
+      <div>
+        <h1>{person.name}</h1>
+        <img
+          src={`${url}/${person.image}`}
+          alt={person.name}
+        />
+      </div>
+    )}
+  </StarWars>,
   document.querySelector("#root")
 )
