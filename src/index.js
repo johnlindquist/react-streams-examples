ec888b61d78bfed2f1f00bf9541ec9aa901391d8
johnlindquist <johnlindquist@gmail.com>
2018-04-26 21:24:37 +0200

    🤗

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { render } from "react-dom"
 import { pipeProps } from "react-streams"
+import { delay, startWith } from "rxjs/operators"
 
-const HelloWorld = pipeProps()
+const HelloWorld = pipeProps(
+  delay(3000),
+  startWith({ greeting: "Wait..." })
+)
 
 render(
-  <HelloWorld greeting="Hello" name="react-streams">
-    {props => (
-      <h1>
-        {props.greeting}, {props.name}
-      </h1>
-    )}
+  <HelloWorld greeting="Hello">
+    {props => <h1>{props.greeting}</h1>}
   </HelloWorld>,
   document.querySelector("#root")
 )
